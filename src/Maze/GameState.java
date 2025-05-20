a7f3dfdb35c82e1e1bbfb0f3bb55368cb6946c35
seajeong <zxcv12354x@gmail.com>
2025-05-20 14:14:42 +0900

    Add GameState: 탐색 경로 및 전체 상태 저장 기능 추가

--- src/Maze/GameState.java
+++ src/Maze/GameState.java
@@ -1,8 +1,26 @@
 package Maze;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameState {
-    MazeGenerator maze;
-    Player player;
-    List<String> log = new ArrayList<>();
-    boolean showFullMap = false;
+    public MazeGenerator maze;
+    public Player player;
+    public List<String> log = new ArrayList<>();
+    public boolean showFullMap = false;
+
+    public GameState(MazeGenerator maze, Player player) {
+        this.maze = maze;
+        this.player = player;
+    }
+
+    // 로그 추가 메서드 (선택사항)
+    public void logMove(String msg) {
+        log.add(msg);
+    }
+
+    // 전체맵 토글
+    public void toggleMapView() {
+        showFullMap = !showFullMap;
+    }
 }
