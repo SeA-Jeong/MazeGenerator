5794a60de4ebdf51d1dc7f61fb7d1b0185f4213f
seajeong <zxcv12354x@gmail.com>
2025-05-20 14:12:06 +0900

    Add Direction: 방향 처리용 enum 및 이동 벡터 제공

--- src/Maze/Direction.java
+++ src/Maze/Direction.java
@@ -1,9 +1,14 @@
 package Maze;
-
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST;
+    UP, DOWN, LEFT, RIGHT;
 
-    public Direction turnLeft() { ... }
-    public Direction turnRight() { ... }
-    public int[] toVector() { ... }  // (dx, dy)
+    public int[] toDelta() {
+        switch (this) {
+            case UP:    return new int[]{0, -1};
+            case DOWN:  return new int[]{0, 1};
+            case LEFT:  return new int[]{-1, 0};
+            case RIGHT: return new int[]{1, 0};
+            default:    return new int[]{0, 0};
+        }
+    }
 }
